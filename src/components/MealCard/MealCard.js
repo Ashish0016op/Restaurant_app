3ac0a07207757f3259946253c20109aa5390dda0
Ashish0016op <raccoonop0016@gmail.com>
2024-09-10 21:37:39 -0700

    form_commit

--- src/components/MealCard/MealCard.js
+++ src/components/MealCard/MealCard.js
@@ -1,55 +1,44 @@
-import React from 'react';
-import './MealCard.css';
+import React from "react";
+import "./MealCard.css";
 const MealCard = () => {
-    const meal=[
-        {name:"Sushi",menu:"Finest Fish and Veggies",price:"$22.99"},
-        {name:"Schnitzel",menu:"A german speciality!",price:"$16.50"},
-        {name:"Barbecue Burger",menu:"American, raw, meaty",price:"$12.99"},
-        {name:"Green Bowl",menu:"Healthy.... and green...",price:"$29.99"},
-        {name:"Dominos",menu:"Fresh and Tasty Pizzas",price:"$24.99"},
-    ]
+  const meal = [
+    { name: "Sushi", menu: "Finest Fish and Veggies", price: "$22.99" },
+    { name: "Schnitzel", menu: "A german speciality!", price: "$16.50" },
+    { name: "Barbecue Burger", menu: "American, raw, meaty", price: "$12.99" },
+    { name: "Green Bowl", menu: "Healthy.... and green...", price: "$29.99" },
+    { name: "Dominos", menu: "Fresh and Tasty Pizzas", price: "$24.99" },
+  ];
   return (
-    <div className='meal_container'>
-      <div className='meal_box'>
-        {
-            meal.map((item,index)=>{
-                return(
-                    <div key={index}>
-                        <p className='res_name'>{item.name}</p>
-                        <p className='res_menu'>{item.menu}</p>
-                        <p className='res_price'>{item.price}</p><hr/>
-                    </div>
-                )
-            })
-        }
-        {/* <div>
-            <p>Sushi</p>
-            <p>Finest Fish and Veggies</p>
-            <p>$22.99</p>
-        </div><hr/>
-        <div>
-            <p>Sushi</p>
-            <p>Finest Fish and Veggies</p>
-            <p>$22.99</p>
-        </div><hr/>
-        <div>
-            <p>Sushi</p>
-            <p>Finest Fish and Veggies</p>
-            <p>$22.99</p>
-        </div><hr/>
-        <div>
-            <p>Sushi</p>
-            <p>Finest Fish and Veggies</p>
-            <p>$22.99</p>
-        </div><hr/>
-        <div>
-            <p>Sushi</p>
-            <p>Finest Fish and Veggies</p>
-            <p>$22.99</p>
-        </div><hr/> */}
+    <div className="meal_container">
+      <div className="meal_box">
+        {meal.map((item, index) => {
+          return (
+            <div key={index}>
+              <div className="res_box">
+              <div>
+                <p className="res_name">{item.name}</p>
+                <p className="res_menu">{item.menu}</p>
+                <p className="res_price">{item.price}</p>
+              </div>
+              <form className="res_add_box">
+                <div className="res_amount">
+                  <p>Amount</p>
+                  <div className="input_box">
+                    <button className="inp_btn">-</button>
+                    <div>1</div>
+                    <button className="inp_btn">+</button>
+                  </div>
+                </div>
+                <button className="add_btn">+Add</button>
+              </form>
+              </div>
+              <hr/>
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MealCard
+export default MealCard;
