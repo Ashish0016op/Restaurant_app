040cad4b7f50b47dd93040b2be0826fb38354ab1
Ashish0016op <raccoonop0016@gmail.com>
2024-09-12 08:11:48 -0700

    Delete_cart_item_commit

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,7 +6,6 @@ import CartContext from '../../store/cart-context';
 const Modal = () => {
   const overlay = document.getElementById('overlays');
   const ctx = useContext(CartContext);
-
   const closeCartHandler = () => {
     ctx.setOpenCart(false);
   };
@@ -15,16 +14,53 @@ for (let i = 0; i < ctx.cartItem.length; i++) {
   totalAmount += ctx.cartItem[i].price * ctx.cartItem[i].quantity;
 }
 
+const decreaseQuantityHandle=(Item)=>{
+  if(Item.quantity===1){
+    return ctx.setCartItem((preItems)=>{
+     return preItems.filter(cartItem=>cartItem.id!==Item.id);
+    })
+  }
+  ctx.setCartItem((prevItems) => {
+    const existingCartItemIndex = prevItems.findIndex(cartItem => cartItem.id === Item.id);
+    if (existingCartItemIndex !== -1) {
+      const updatedCartItems = [...prevItems];
+      updatedCartItems[existingCartItemIndex].quantity -= 1;
+      return updatedCartItems;
+    }
+  });
+}
+const IncreseQuantityHandle=(Item)=>{
+  ctx.setCartItem((prevItems) => {
+    const existingCartItemIndex = prevItems.findIndex(cartItem => cartItem.id === Item.id);
+    
+    if (existingCartItemIndex !== -1) {
+      const updatedCartItems = [...prevItems];
+      updatedCartItems[existingCartItemIndex].quantity += 1;
+      return updatedCartItems;
+    }
+  });
+}
   return createPortal(
     <div className='modal'>
       <div className='modal-content'>
         {ctx.cartItem.length > 0 ? (
           <>
             {ctx.cartItem.map((item, index) => (
-              <div key={index} className='cart-item'>
+              <div key={index}>
+              <div className='cart_box'>
+              <div className='cart-item'>
                 <p className='cart_res_name'>{item.name}</p>
-                <p className='cart_item_qty'>x {item.quantity}</p>
+                <div className='quantity_price_box'>
                 <p className='cart_item_price'>${(item.price * item.quantity).toFixed(2)}</p>
+                  <p className='cart_item_qty'>x {item.quantity}</p>
+                </div>
+              </div>
+              <div className='btn_box'>
+                <button onClick={()=>decreaseQuantityHandle(item)}>-</button>
+                <button onClick={()=>IncreseQuantityHandle(item)}>+</button>
+              </div>
+              </div>
+              <hr/>
               </div>
             ))}
             <div className='cart_amt_box'>
